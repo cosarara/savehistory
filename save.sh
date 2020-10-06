ec7e54ba7f95399074490b6c178241d43f3695b3
Jaume Delclòs Coll <cosarara@cosarara.me>
2020-10-06 18:26:51 +0200

    json for the jq fans

--- save.sh
+++ save.sh
@@ -2,12 +2,14 @@
 set -eu
 outdir=~/documents/history
 outfile="${outdir}/backup_$(date -Iseconds).txt"
+outjson="${outdir}/backup_$(date -Iseconds).json"
 cd "$(dirname "$0")"
 profile=$(./get_profile.py)
 cd ~/.mozilla/firefox/
 cd "$profile"
 tmpfile=$(mktemp /tmp/firefox-history-backup.XXXXXX.sqlite)
 cp places.sqlite "$tmpfile"
-sqlite3 "$tmpfile" 'select url,title from moz_places, moz_historyvisits where moz_places.id = moz_historyvisits.place_id group by url;' > "$outfile"
-echo "saved to $outfile"
+sqlite3 "$tmpfile" 'select datetime(visit_date/1000000,"unixepoch"),url,title from moz_places, moz_historyvisits where moz_places.id = moz_historyvisits.place_id group by url;' > "$outfile"
+sqlite3 "$tmpfile" "select json_object('date', datetime(visit_date/1000000,'unixepoch'), 'url', url, 'title', title) from moz_places, moz_historyvisits where moz_places.id = moz_historyvisits.place_id group by url;" > "$outjson"
+echo "saved to $outfile, $outjson"
 rm "$tmpfile"
